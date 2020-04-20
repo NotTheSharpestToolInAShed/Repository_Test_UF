face8c1809f32e58ca70f114c0f66f3cc3336323
BufficBoy104 <58601146+BufficBoy104@users.noreply.github.com>
2020-04-20 18:50:25 +0300

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine("Hello World"); // у меня нет скачанного дополнения для Python, поэтому на C#
+    int a = 4;
+    int b =11;
+    int q = 5;
+    int c = (a + b)/q;
+    Console.WriteLine(c)
+    
+  }
+}
